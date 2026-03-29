2bfdbe0605541fdf216ec94609b6375087dd0954
Saksham <sakshambhadwal1@gmail.com>
2026-03-29 12:10:00 +0530

    digital clock

---
+++ projects/digital-clock/src/App.jsx
@@ -0,0 +1,14 @@
+
+import Clock from './clock'
+
+function App() {
+  
+
+  return (
+  <>
+  <Clock></Clock>
+  </>
+  )
+}
+
+export default App
---
+++ projects/digital-clock/src/clock.jsx
@@ -0,0 +1,31 @@
+import { useState, useEffect } from "react"
+
+function Clock() {
+    const [hour, sethour] = useState();
+    const [min, setmin] = useState();
+    const [sec, setsec] = useState();
+    const meridiem = hour >=12 ? "PM" : "AM"
+
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            const now = new Date()
+            sethour(now.getHours())
+            setmin(now.getMinutes())
+            setsec(now.getSeconds())
+        }, 1000);
+        return () => clearInterval(intervalId);
+    }, [])
+
+    function padzero(number) {
+        return (number<10 ? "0": "")+ number
+    }
+
+
+    return (
+        <>
+            <h1>time:{hour} : {min} : {padzero(sec)} {meridiem}</h1>
+        </>
+    )
+}
+
+export default Clock

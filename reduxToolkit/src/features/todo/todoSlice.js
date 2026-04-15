ab1117083cc66144059426fe7a020d4a4531b6a4
Saksham <sakshambhadwal1@gmail.com>
2026-04-15 22:17:54 +0530

    update in todo

--- reduxToolkit/src/features/todo/todoSlice.js
+++ reduxToolkit/src/features/todo/todoSlice.js
@@ -1,25 +1,31 @@
-import { createSlice,nanoid } from "@reduxjs/toolkit";
+import { createSlice, nanoid } from "@reduxjs/toolkit";
 
-const initialState ={
+const initialState = {
     todos: []
 }
 
 export const todoSlice = createSlice({
     name: 'todo',
     initialState,
-    reducers:{
-        addTodo: (state, action) =>{
-            const todo ={
+    reducers: {
+        addTodo: (state, action) => {
+            const todo = {
                 id: nanoid(), text: action.payload
             }
             state.todos.push(todo)
         },
-        removeTodo: (state, action) =>{
-          state.todos = state.todos.filter((todo) => todo.id !== action.payload)
+        removeTodo: (state, action) => {
+            state.todos = state.todos.filter((todo) => todo.id !== action.payload)
+        },
+        editTodo: (state, action) => {
+            const todo = state.todos.find((t) => t.id === action.payload.id)
+            if (todo) {
+                todo.text = action.payload.text
+            }
         }
     }
 })
 
-export const {addTodo, removeTodo} = todoSlice.actions
+export const { addTodo, removeTodo, editTodo } = todoSlice.actions
 
 export default todoSlice.reducer
